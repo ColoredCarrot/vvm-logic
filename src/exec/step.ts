deca44c9e13787842cde44b2da120a635c67e166
julian <7807570+ColoredCarrot@users.noreply.github.com>
2022-11-07 16:48:22 +0100

    Fix lint

--- src/exec/step.ts
+++ src/exec/step.ts
@@ -4,5 +4,5 @@ import {State} from "../model/State";
  * Step from one machine state to the next by parsing and processing the given instruction.
  */
 export function step(state: State, instruction: string): State {
-    return {...state, output: state.output + "a"};
+    return {...state, output: state.output + " " + instruction};
 }
