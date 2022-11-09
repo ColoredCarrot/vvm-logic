c8fd7917c83886ed9902e6460d0a9de702236140
hannahkiel <hannah.kiel@tum.de>
2022-11-09 12:49:38 +0100

    getter und setter methoden für Pointer und ProgramCounter

--- src/model/State.ts
+++ src/model/State.ts
@@ -16,4 +16,46 @@ export class State {
     trailPointer = 0;
     programCounter = 0;
     // stackPointer = stack.length
+
+    getFramePointer(): number {
+        return this.framePointer;
+    }
+
+    setFramePointer(framePointer: number){
+        this.framePointer = framePointer;
+    }
+
+    getBacktrackPointer(): number{
+        return this.backtrackPointer;
+    }
+
+    setBacktrackPointer(backtrackPointer: number): void{
+        this.backtrackPointer = backtrackPointer;
+    }
+
+    getTrailPointer(): number{
+        return this.trailPointer;
+    }
+
+    setTrailPointer(trailPointer: number): void{
+        this.trailPointer = trailPointer;
+    }
+
+    getHeapPointer(): number{
+        return this.heapPointer;
+    }
+
+    setHeapPointer(heapPointer: number): void{
+        this.heapPointer = heapPointer;
+    }
+
+    getProgramCounter(): number{
+        return this.programCounter;
+    }
+
+    setProgramCounter(programCounter: number){
+        this.programCounter = programCounter;
+    }
+
+
 }
