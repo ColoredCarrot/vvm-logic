c9bd95b8050e98093a34fdf4cca924d32e80c133
Jakob <jakob.zuchna@tum.de>
2022-11-09 16:50:30 +0100

    Adjust Label classes and adjust Call constructor

--- src/exec/Label.ts
+++ src/exec/Label.ts
@@ -1,11 +1,18 @@
 export class Label {
-    line : number
-    text : string
+    private _line : number
+    private _text : string
 
     constructor(line: number, text: string) {
-        this.line = line;
-        this.text = text;
+        this._line = line;
+        this._text = text;
     }
 
 
+    get line(): number {
+        return this._line;
+    }
+
+    get text(): string {
+        return this._text;
+    }
 }
