0ab30e5890287ebf71677a0af1ed4a5eb37a4936
Jakob <jakob.zuchna@tum.de>
2022-11-09 19:58:20 +0100

    Add p.140 example

--- src/__tests__/InstructionParser.test.tsx
+++ src/__tests__/InstructionParser.test.tsx
@@ -1,29 +1,51 @@
 import {InstructionParser} from "../exec/InstructionParser";
 import {Label} from "../exec/Label";
 
+let page140Example : string [] =
+        ["init N",
+    "pushenv 0",
+    "mark A",
+    "call p/0",
+    "A: halt 0",
+    "N: no",
+    "t/1: pushenv 1",
+    "putref 1",
+    "uatom b",
+    "popenv",
+    "p/0: pushenv 1",
+    "mark B",
+    "putvar 1",
+    "call q/1",
+    "B: mark C",
+    "putref 1",
+    "call t/1",
+    "C: popenv",
+    "q/1: pushenv 1",
+    "mark D",
+    "putref 1",
+    "call s/1",
+    "D: popenv",
+    "s/1: setbtp",
+    "try E",
+    "delbtp",
+    "jump F",
+    "E: pushenv 1",
+    "mark G",
+    "putref 1",
+    "call t/1",
+    "G: popenv",
+    "F: pushenv 1",
+    "putref 1",
+    "uatom a",
+    "popenv"]
 
-test('example with only label check for instructionparser', () => {
-        let inputLines : string[];
-        inputLines =
-                [
-                    "A: call B",
-                    "B: call A",
-                    "g/2: jump A",
-                    "pop",
-                    "unify"
-                ];
-        let expectedLabels : Label[] =
-            [
-                new Label(0, "A"),
-                new Label(1, "B"),
-                new Label(2, "g/2")
-            ]
-
-
-        let result = InstructionParser.parseInput(inputLines);
+test('page 140 example no check', () => {
+        let result = InstructionParser.parseInput(page140Example);
         let instructions = result.at(0)!;
         let labels = result.at(1)!;
 
-        expect(labels).toStrictEqual(expectedLabels);
+        console.log("Parsing finished")
+        console.log(labels)
+        console.log(instructions)
 });
 
