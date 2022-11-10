d1f532ec03da5f6aaf0652b49d1ea7c0c8169c39
Jakob <jakob.zuchna@tum.de>
2022-11-10 16:34:19 +0100

    Implement Slide.ts

--- src/exec/instructions/Slide.ts
+++ src/exec/instructions/Slide.ts
@@ -1,14 +1,31 @@
 import {Instruction} from "./Instruction";
 import {State} from "../../model/State";
+import {Cell} from "../../model/Cell";
 
 export class Slide extends Instruction {
-    //TODO: Just dummy of class
+    m: number;
+    h: number;
 
     constructor(m: number, h: number) {
-        super("SLIDE");
+        super("SLIDE " + m + " " + h);
+        this.m = m;
+        this.h = h;
     }
 
     step(state: State): State {
+        const refs: Cell[] = [];
+        for (let i = 0; i < this.h; i++) {
+            refs.push(state.stack.pop());
+        }
+
+        for (let i = 0; i < this.m; i++) {
+            state.stack.pop();
+        }
+
+        for (let i = 0; i < this.h; i++) {
+            state.stack.push(refs.pop()!);
+        }
+
         return state;
     }
 }
