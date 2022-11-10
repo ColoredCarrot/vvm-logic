488bf88c69d2389360e20bb907fb2dc249939cbe
hannahkiel <hannah.kiel@tum.de>
2022-11-10 14:40:36 +0100

    compketed new instructions: mark, popenv, pushenv

--- src/exec/instructions/Init.ts
+++ src/exec/instructions/Init.ts
@@ -1,5 +1,7 @@
 import {Instruction} from "./Instruction";
 import {State} from "../../model/State";
+import {ValueCell} from "../../model/ValueCell";
+import {UninitializedCell} from "../../model/UninitializedCell";
 
 export class Init extends Instruction {
 
@@ -11,6 +13,15 @@ export class Init extends Instruction {
     }
 
     step(state: State): State {
+        state.setFramePointer(5);
+        state.setBacktrackPointer(5);
+
+        state.stack.push(new ValueCell(this.addr));
+        state.stack.push(new ValueCell(-1));
+        state.stack.push(new ValueCell(-1));
+        state.stack.push(new ValueCell(0));
+        state.stack.push(new UninitializedCell());
+        state.stack.push(new UninitializedCell());
 
         return state;
     }
