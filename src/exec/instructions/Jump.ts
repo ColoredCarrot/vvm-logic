7ec121a75e4f1a0f53c4568ca3ea5bb519b2fe15
Jakob <jakob.zuchna@tum.de>
2022-11-10 15:00:49 +0100

    Implement Jump

---
+++ src/exec/instructions/Jump.ts
@@ -0,0 +1,17 @@
+import {Instruction} from "./Instruction";
+import {Label} from "../Label";
+import {State} from "../../model/State";
+
+export class Jump extends Instruction{
+
+    label : Label;
+
+    constructor(label : Label) {
+        super("jump" + label.text);
+        this.label = label;
+    }
+
+    step(state: State): State {
+        return state.setProgramCounter(this.label.line);
+    }
+}
