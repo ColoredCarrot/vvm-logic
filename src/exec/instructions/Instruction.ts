047b7eda786c3143561254126c24b4b27902738a
Jakob <jakob.zuchna@tum.de>
2022-11-10 10:05:02 +0100

    Add Instruction method dummy methods

--- src/exec/instructions/Instruction.ts
+++ src/exec/instructions/Instruction.ts
@@ -4,4 +4,28 @@ export abstract class Instruction {
     protected constructor(public instruction: string) {}
 
     abstract step(state: State): State;
+
+    public static unify(state : State, u : number, v : number) : boolean {
+        return false;
+    }
+
+    public static trail (state : State, u : number) {
+
+    }
+
+    public static backtrack(state : State) {
+
+    }
+
+    public static reset(state : State, x : number, y : number) {
+
+    }
+
+    public static check(state : State, u : number, v : number) : boolean {
+        return false;
+    }
+
+    public static deref(state : State, v : number) : number {
+        return 0;
+    }
 }
