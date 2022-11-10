6a87786727926a636c06731e34b3628dcd1f3aea
hannahkiel <hannah.kiel@tum.de>
2022-11-10 09:44:21 +0100

    -Uninitialized Cell

--- src/model/ValueCell.ts
+++ src/model/ValueCell.ts
@@ -1,16 +1,11 @@
 import {Cell} from "./Cell";
 
 export class ValueCell extends Cell {
-    value: number | undefined;
+    value: number;
 
-    constructor(value: number | undefined) {
+    constructor(value: number) {
         super();
-
-        if (value) {
-            this.value = value;
-        } else {
-            this.value = undefined;
-        }
+        this.value = value;
     }
 }
 
