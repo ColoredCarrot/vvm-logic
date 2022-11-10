90a33af10560acc6824fa5aba5b5411f52d2d77b
Mirella Bärligea <baerli9120@gmail.com>
2022-11-10 10:46:42 +0100

    fix putanon and putatom instruction

--- src/exec/instructions/Putanon.ts
+++ src/exec/instructions/Putanon.ts
@@ -1,9 +1,8 @@
 import {Instruction} from "./Instruction";
 import {State} from "../../model/State";
 import {VariableCell} from "../../model/VariableCell";
-import {Cell} from "../../model/Cell";
-import {Heap} from "../../model/Heap";
 import {UninitializedCell} from "../../model/UninitializedCell";
+import {PointerToHeapCell} from "../../model/PointerToHeapCell";
 
 export class Putanon extends Instruction {
 
@@ -13,15 +12,9 @@ export class Putanon extends Instruction {
 
     step(state: State): State {
 
-        // allokiere Platz für eine Zelle auf Heap
-        // lege Cell mit Typ Variable auf den Heap mit Wert der Adresse von Heap (= Selbstreferenz)
-        // increase the HeapPointer um 1 (macht Heap!)
-
         let [newHeap, address] = state.heap.alloc([new UninitializedCell()])
-
         state.heap = newHeap.set(address, new VariableCell(address))
-
-        //state.stack.push(Adresse von Heap, ist Pointer zu Heap)
+        state.stack.push(new PointerToHeapCell(address))
 
         return state;
     }
