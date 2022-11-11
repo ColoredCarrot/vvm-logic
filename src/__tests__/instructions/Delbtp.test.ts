81a16ce490c11283faf1240a233c13d26ffac106
hannahkiel <hannah.kiel@tum.de>
2022-11-11 17:18:52 +0100

    instruction tests: init test, pop test, popenv test, prune test

--- src/__tests__/instructions/Delbtp.test.ts
+++ src/__tests__/instructions/Delbtp.test.ts
@@ -9,13 +9,13 @@ test("Instruction: DELBTP", () => {
 
 
     const prevState = State.new()
-        .setStack(Stack.of(new UninitializedCell(),new UninitializedCell(),new UninitializedCell(),new UninitializedCell(),
-            new PointerToStackCell(2),new UninitializedCell(),new UninitializedCell(),new UninitializedCell(), new UninitializedCell(),new UninitializedCell()))
+        .setStack(Stack.of(new UninitializedCell(), new UninitializedCell(), new UninitializedCell(), new UninitializedCell(),
+            new PointerToStackCell(2), new UninitializedCell(), new UninitializedCell(), new UninitializedCell(), new UninitializedCell(), new UninitializedCell()))
         .setFramePointer(8).setBacktrackPointer(8);
 
     const expectedState = State.new()
-        .setStack(Stack.of(new UninitializedCell(),new UninitializedCell(),new UninitializedCell(),new UninitializedCell(),
-            new PointerToStackCell(2),new UninitializedCell(),new UninitializedCell(),new UninitializedCell(), new UninitializedCell(),new UninitializedCell()))
+        .setStack(Stack.of(new UninitializedCell(), new UninitializedCell(), new UninitializedCell(), new UninitializedCell(),
+            new PointerToStackCell(2), new UninitializedCell(), new UninitializedCell(), new UninitializedCell(), new UninitializedCell(), new UninitializedCell()))
         .setFramePointer(8).setBacktrackPointer(2);
 
 
