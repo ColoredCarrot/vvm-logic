a20bdb59db5823d4014b0b1d684b8e3aef1361bb
Mirella Bärligea <baerli9120@gmail.com>
2022-11-11 16:55:14 +0100

    add call test

---
+++ src/__tests__/instructions/Call.test.ts
@@ -0,0 +1,24 @@
+import {UninitializedCell} from "../../model/UninitializedCell";
+import {ValueCell} from "../../model/ValueCell";
+import {Stack} from "../../model/Stack";
+import {PointerToHeapCell} from "../../model/PointerToHeapCell";
+import { Heap } from "../../model/Heap";
+import { State } from "../../model/State";
+import {Call} from "../../exec/instructions/Call";
+import {VariableCell} from "../../model/VariableCell";
+import {Trail} from "../../model/Trail";
+import {SignLabel} from "../../exec/SignLabel";
+
+test("Instruction: CALL", () => {
+    const instr = new Call(new SignLabel(2, "p/2"));
+
+    const prevState = State.new()
+        .setStack(Stack.of(new UninitializedCell(), new ValueCell(123), new UninitializedCell(), new UninitializedCell()));
+
+    const expectedState = State.new()
+        .setStack(Stack.of(new UninitializedCell(), new ValueCell(123), new UninitializedCell(), new UninitializedCell()))
+        .setFramePointer(1)
+        .setProgramCounter(2);
+
+    expect(instr.step(prevState)).toStrictEqual(expectedState);
+});
