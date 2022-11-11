57745d653730cfe0dd29e55aab4395fb91616e8a
hannahkiel <hannah.kiel@tum.de>
2022-11-11 09:25:23 +0100

    new instructions: updates

--- src/exec/instructions/Init.ts
+++ src/exec/instructions/Init.ts
@@ -2,14 +2,15 @@ import {Instruction} from "./Instruction";
 import {State} from "../../model/State";
 import {ValueCell} from "../../model/ValueCell";
 import {UninitializedCell} from "../../model/UninitializedCell";
+import {Label} from "../Label";
 
 export class Init extends Instruction {
 
-    private readonly addr: number;
+    private readonly label: Label;
 
-    constructor(addr: number) {
+    constructor(label: Label) {
         super("INIT");
-        this.addr = addr;
+        this.label = label;
     }
 
     step(state: State): State {
@@ -17,7 +18,7 @@ export class Init extends Instruction {
         state = state.setBacktrackPointer(5);
 
 
-        state = state.pushStack(new ValueCell(this.addr));
+        state = state.pushStack(new ValueCell(this.label.line));
         state = state.pushStack(new ValueCell(-1));
         state = state.pushStack(new ValueCell(-1));
         state = state.pushStack(new ValueCell(0));
