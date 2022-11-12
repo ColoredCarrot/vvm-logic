77db317f9cf3a9c52ddb3d238853c8b9ad1c0239
Mirella Bärligea <baerli9120@gmail.com>
2022-11-12 11:15:28 +0100

    add jump test

---
+++ src/__tests__/instructions/Jump.test.ts
@@ -0,0 +1,15 @@
+import {State} from "../../model/State";
+import {Jump} from "../../exec/instructions/Jump";
+import {Label} from "../../exec/Label";
+
+test("Instruction: JUMP", () => {
+    const instr = new Jump(new Label(5, "B"));
+
+    const prevState = State.new()
+        .setProgramCounter(1)
+
+    const expectedState = State.new()
+        .setProgramCounter(5);
+
+    expect(instr.step(prevState)).toStrictEqual(expectedState);
+});
