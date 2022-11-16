d12a3a0a290b33d63da30f888bba4fbc99cbdcbb
Jakob <jakob.zuchna@tum.de>
2022-11-16 11:54:13 +0100

    Stylesheet

--- src/view/visualization/Visualization.tsx
+++ src/view/visualization/Visualization.tsx
@@ -1,14 +1,17 @@
-import React, {useEffect, useLayoutEffect, useRef} from "react";
+import React, {useLayoutEffect, useRef} from "react";
 import {State} from "../../model/State";
-import Cytoscape, {Position} from "cytoscape";
+import Cytoscape from "cytoscape";
+import cytoscape from "cytoscape";
 import Cola from "cytoscape-cola";
 import Avsdf from "cytoscape-avsdf";
 import CytoscapeComponent from "react-cytoscapejs";
 import "./Visualization.scss";
 import "springy/springy";
+import fcose from "cytoscape-fcose";
 
 Cytoscape.use(Cola);
 Cytoscape.use(Avsdf);
+Cytoscape.use(fcose);
 
 interface VisualizationProps {
     prevState: State;
@@ -23,6 +26,30 @@ export function Visualization({prevState, state}: VisualizationProps) {
     </div>;
 }
 
+function generateLayout(state: State, cy: React.MutableRefObject<Cytoscape.Core | undefined>): cytoscape.LayoutOptions {
+    //Try with cose
+    /*
+        cytoscape.use(fcose);
+        let aConstr = JSO
+        let stackVertAlignment = "[";
+        for (let i = 0; i < state.stack.size; i++) {
+            stackVertAlignment = stackVertAlignment + "\"s" + i + "\"";
+        }
+
+        stackVertAlignment = stackVertAlignment + "]";
+
+        const alignmentConstraint = "{vertical: [" + stackVertAlignment + "]";
+        const aConstr = JSON.parse(alignmentConstraint);
+    */
+
+    return {
+        name: "fcose",
+        // @ts-ignore
+        animate: true,
+        randomize: false,
+        //alignment: {vertical: [["FP", "BP", "PC"]]},
+    };
+}
 
 function VisualizationGraph({state}: VisualizationProps) {
 
@@ -31,15 +58,8 @@ function VisualizationGraph({state}: VisualizationProps) {
     useLayoutEffect(() => {
         //cy.current!.$id("s3").positions({x: 300, y: 300}).lock();
         cy.current!
-            .elements("[type = 'heap']")
-            .layout(
-                {
-                    name: "cola",
-                    // @ts-ignore
-                    animate: true,
-                    randomize: false,
-                }
-            ).run();
+            .layout(generateLayout(state, cy)).run();
+
         // cy.current!
         //     .elements("node[type = 'stack']")
         //     .layout({
@@ -50,15 +70,34 @@ function VisualizationGraph({state}: VisualizationProps) {
         //     .run();
     });
 
-    const a = [];
+    const nodes = [];
     const edges = [];
+
+    const programmCounter = state.programCounter;
+    nodes.push(
+        {
+            data: {id: "PC", label: programmCounter, type: "register-value"},
+            style: {label: programmCounter},
+        });
+    const framePointer = state.framePointer;
+    nodes.push({
+        data: {id: "FP", label: framePointer, type: "register-value"},
+        style: {label: framePointer},
+    });
+
+    const backtrackPointer = state.backtrackPointer;
+    nodes.push({
+        data: {id: "BP", label: backtrackPointer, type: "register-value"},
+        style: {label: backtrackPointer},
+    });
+
     for (let i = 0; i < state.stack.size; ++i) {
         const cell = state.stack.get(i);
 
         if (i < 5) {
-            a.push({data: {id: "s" + i, label: "Node " + i, type: "stack"}, position: {x: 100, y: 100 * i}});
+            nodes.push({data: {id: "s" + i, label: "Node " + i, type: "stack"}, position: {x: 100, y: 100 * i}});
         } else {
-            a.push({data: {id: "s" + i, label: "Node " + i, type: "heap"}});
+            nodes.push({data: {id: "s" + i, label: "Node " + i, type: "heap"}});
             edges.push({
                 data: {
                     id: "e" + i,
@@ -70,11 +109,34 @@ function VisualizationGraph({state}: VisualizationProps) {
         }
     }
 
+
     return <CytoscapeComponent
         cy={theCy => {
             cy.current = theCy;
         }}
         style={{width: "100%", height: "100%"}}
+        stylesheet={[
+            {
+                selector: "node",
+                style: {
+                    width: 50,
+                    height: 20,
+                    shape: "rectangle",
+                },
+            },
+            {
+                selector: "[type='register-value']",
+                style: {
+                    backgroundColor: "blue",
+                },
+            },
+            {
+                selector: "edge",
+                style: {
+                    width: 15,
+                },
+            },
+        ]}
         elements={CytoscapeComponent.normalizeElements({
             nodes: a,
             edges: edges,
