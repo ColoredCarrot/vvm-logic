049caff212c936f53d0045cbe360a4863b8e025e
Mirella Bärligea <baerli9120@gmail.com>
2022-11-16 15:00:42 +0100

    create logic

--- src/view/visualization/Visualization.tsx
+++ src/view/visualization/Visualization.tsx
@@ -8,6 +8,15 @@ import CytoscapeComponent from "react-cytoscapejs";
 import "./Visualization.scss";
 import "springy/springy";
 import fcose from "cytoscape-fcose";
+import {UninitializedCell} from "../../model/UninitializedCell";
+import { ValueCell } from "../../model/ValueCell";
+import {PointerToStackCell} from "../../model/PointerToStackCell";
+import {PointerToHeapCell} from "../../model/PointerToHeapCell";
+import {stat} from "fs";
+import {Address} from "cluster";
+import { AtomCell } from "../../model/AtomCell";
+import {VariableCell} from "../../model/VariableCell";
+import { StructCell } from "../../model/StructCell";
 
 Cytoscape.use(Cola);
 Cytoscape.use(Avsdf);
@@ -78,9 +87,9 @@ function VisualizationGraph({state}: VisualizationProps) {
     const nodes = [];
     const edges = [];
 
+    // REGISTER
     const programmCounter = state.programCounter;
-    nodes.push(
-        {
+    nodes.push({
             data: {id: "PC", label: programmCounter, type: "register-value"},
             style: {label: programmCounter},
         });
@@ -89,29 +98,127 @@ function VisualizationGraph({state}: VisualizationProps) {
         data: {id: "FP", label: framePointer, type: "register-value"},
         style: {label: framePointer},
     });
-
+    if (framePointer != -1){
+        edges.push({
+            data: {
+                id: "FP",
+                source: "FP",
+                target: "S" + framePointer,
+                type: "register-pointer",
+            },
+        });
+    }
     const backtrackPointer = state.backtrackPointer;
     nodes.push({
         data: {id: "BP", label: backtrackPointer, type: "register-value"},
         style: {label: backtrackPointer},
     });
+    if (backtrackPointer != -1){
+        edges.push({
+            data: {
+                id: "BP",
+                source: "BP",
+                target: "S" + backtrackPointer,
+                type: "register-pointer",
+            },
+        });
+    }
 
+    // STACK
     for (let i = 0; i < state.stack.size; ++i) {
-        const cell = state.stack.get(i);
+        const stackCell = state.stack.get(i);
 
-        if (i < 5) {
-            nodes.push({data: {id: "s" + i, label: "Node " + i, type: "stack"}, position: {x: 100, y: 100 * i}});
-        } else {
-            nodes.push({data: {id: "s" + i, label: "Node " + i, type: "heap"}});
+        if (stackCell instanceof UninitializedCell) {
+            nodes.push({data: {id: "S" + i, label: "Stack[" + i +"]", type: "stack-uninitialized"}});
+        }
+        else if (stackCell instanceof ValueCell) {
+            nodes.push({
+                data: {id: "S" + i, label: "Stack[" + i +"]", type: "stack-value"},
+                style: {label: stackCell.value}
+            });
+        }
+        else if (stackCell instanceof PointerToStackCell) {
+            nodes.push({data: {id: "S" + i, label: "Stack[" + i +"]", type: "stack-pointerToStack"}});
             edges.push({
                 data: {
-                    id: "e" + i,
-                    source: "s" + i,
-                    target: "s" + Math.floor(Math.random() * i),
-                    type: "heap",
+                    id: "SP" + i,
+                    source: "S" + i,
+                    target: "S" + stackCell.value,
+                    type: "stack-pointerToStack",
                 },
             });
         }
+        else if (stackCell instanceof PointerToHeapCell) {
+            nodes.push({data: {id: "S" + i, label: "Stack[" + i +"]", type: "stack-pointerToHeap"}});
+            edges.push({
+                data: {
+                    id: "SP" + i,
+                    source: "S" + i,
+                    target: "H" + stackCell.value,
+                    type: "stack-pointerToHeap",
+                },
+            });
+        }
+    }
+
+    // HEAP
+    for (let i = 0; i < state.heap.getHeapPointer(); ++i){
+
+        // nur wenn an Heapadresse etwas steht!
+        if (state.heap.get(i)){
+
+            const heapCell = state.heap.get(i);
+
+            if (heapCell instanceof UninitializedCell) {
+                nodes.push({data: {id: "H" + i, label: "Heap[" + i +"]", type: "heap-uninitialized"}});
+            }
+            else if (heapCell instanceof AtomCell) {
+                nodes.push({
+                    data: {id: "H" + i, label: "Heap[" + i +"]", type: "heap-atom"},
+                    style: {label: "A " + heapCell.value}
+                });
+            }
+            else if (heapCell instanceof VariableCell) {
+                nodes.push({
+                    data: {id: "H" + i, label: "Heap[" + i +"]", type: "heap-variable"},
+                    style: {label: heapCell.tag + " " + heapCell.value}
+                });
+                edges.push({
+                    data: {
+                        id: "HP" + i,
+                        source: "H" + i,
+                        target: "H" + heapCell.value,
+                        type: "heap-pointerToHeap",
+                    },
+                });
+            }
+            else if (heapCell instanceof StructCell) {
+                nodes.push({
+                    data: {id: "H" + i, label: "Heap[" + i +"]", type: "heap-struct"},
+                    style: {label: "S " + heapCell.label}
+                });
+            }
+            else if (heapCell instanceof PointerToHeapCell) {
+                nodes.push({data: {id: "H" + i, label: "Heap[" + i +"]", type: "heap-pointerToHeap"}});
+                edges.push({
+                    data: {
+                        id: "HP" + i,
+                        source: "H" + i,
+                        target: "H" + heapCell.value,
+                        type: "heap-pointerToHeap",
+                    },
+                });
+            }
+        }
+        // sonst einfach weiter Heap durchgehen!
+    }
+
+    // TRAIL (if exists)
+    for (let i = 0; i < state.trail.trailPointer; ++i) {
+        nodes.push({
+            data: {id: "T"+i, label: "Trail["+i+"]", type: "trail-value"},
+            style: {label: state.trail.get(i)}
+        })
     }
 
 
