57c40bf1f9ff4d7828cc6ff7b0cf2d849b90ee64
jzuchna <zuchna.jakob@gmail.com>
2022-11-21 12:48:43 +0100

    Implement BindingsFor Method. Creates string that represents bindings

--- src/exec/instructions/Halt.ts
+++ src/exec/instructions/Halt.ts
@@ -1,8 +1,10 @@
 import {Instruction} from "./Instruction";
 import {State} from "../../model/State";
-import Immutable, {OrderedMap} from "immutable";
 import {PointerToHeapCell} from "../../model/PointerToHeapCell";
 import {ExecutionError} from "../ExecutionError";
+import {AtomCell} from "../../model/AtomCell";
+import {VariableCell} from "../../model/VariableCell";
+import {StructCell} from "../../model/StructCell";
 
 export class Halt extends Instruction {
     readonly d: number;
@@ -13,7 +15,7 @@ export class Halt extends Instruction {
     }
 
     step(state: State): State {
-        const bindings: Map<number, string> = new Map<number, string>();
+        const bindings: string[] = [];
         if (this.d != 0) {
             for (let i = 6; i < 6 + this.d; i++) {
                 const cellOnStack = state.stack.get(i);
@@ -21,17 +23,49 @@ export class Halt extends Instruction {
                     throw new ExecutionError("Expected Cells at Result to point to Heap");
                 } else {
                     const res = Instruction.deref(state, cellOnStack.value);
-                    bindings.set(i, this.bindingsFor(state, res));
+                    bindings.push(this.bindingsFor(state, res));
                 }
             }
         }
-        //TODO: Add Bingings to State and show state it needs to present them
 
         return state;
     }
 
     private bindingsFor(state: State, addr: number): string {
+        const result = state.heap.get(addr);
+        if (result instanceof AtomCell) {
+            return result.value;
+        }
+        if (result instanceof VariableCell) {
+            return "_" + result.value;
+        }
+        if (result instanceof StructCell) {
+            const name = result.label;
+            if (name === "[|]/2") {
+                //Special case for binary Constructor
+                const deref1 = Instruction.deref(state, addr + 1);
+                const deref2 = Instruction.deref(state, addr + 2);
+                return "[" + this.bindingsFor(state, deref1) + "|" + this.bindingsFor(state, deref2) + "]";
+            } else {
+                //General Structure
+                const nameWOSize = name.split("/")[0];
+                let ret = nameWOSize + "(";
+                for (let i = 1; i < result.size; i++) {
+                    //Add Bindings for all struct cells
+                    const derefI = Instruction.deref(state, addr + i);
+                    ret += this.bindingsFor(state, derefI);
+                    ret += ", ";
+                }
+                //Add Binding for last struct cell
+                const derefLast = Instruction.deref(state, addr + result.size);
+                ret += this.bindingsFor(state, derefLast);
+                ret += ")";
+                return ret;
+            }
 
-        return "";
+
+        }
+
+        throw new ExecutionError("Halt expects Reference to Variable, Atom or struct, but was " + JSON.stringify(result));
     }
 }
