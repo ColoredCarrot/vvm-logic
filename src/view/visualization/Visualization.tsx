c821d2840dc829f776a386a924e570c99f3b80f3
julian <7807570+ColoredCarrot@users.noreply.github.com>
2022-11-21 15:28:32 +0100

    Resolve #76: Implement dialogs

--- src/view/visualization/Visualization.tsx
+++ src/view/visualization/Visualization.tsx
@@ -2,30 +2,83 @@ import Cytoscape from "cytoscape";
 import cytoscape from "cytoscape";
 import cytoscapeFcose from "cytoscape-fcose";
 import fcose, {FcoseLayoutOptions} from "cytoscape-fcose";
-import React, {useRef} from "react";
+import React, {useContext, useRef} from "react";
 import CytoscapeComponent from "react-cytoscapejs";
 import {State} from "../../model/State";
 import {STYLESHEET, TOTAL_NODE_HEIGHT} from "./NodeStyles";
 import "./Visualization.scss";
 import {createGraph, Graph} from "./VisualizationGraph";
+import {Dialog} from "../../model/dialog/Dialog";
+import {AppState, AppStateContext} from "../AppState";
+import {ExecutionError} from "../../exec/ExecutionError";
 
 Cytoscape.use(fcose);
 
-interface VisualizationProps {
-    state: State;
-}
-
-export function Visualization({state}: VisualizationProps) {
+export function Visualization() {
 
     /////////////
     // Don't display heap cells whose address is >= heap pointer
     /////////////
 
+    const [appState, setAppState] = useContext(AppStateContext);
+    const state = appState.vmState.last() ?? State.new();
+
     return <div className="Visualization">
         <h3>Visualization</h3>
 
         <VisualizationGraph state={state}/>
+
+        <div className={"Visualization__Modal" + (state.activeDialog !== null ? " Visualization__Modal--shown" : "")}>
+            <div className="Visualization__Modal__Dialog">
+                <div className="Visualization__Modal__Dialog__Content">
+                    {state.activeDialog?.renderContent()}
+                </div>
+                <div className="Visualization__Modal__Dialog__Buttons">
+                    {(state.activeDialog?.choices as readonly string[] ?? [])?.map(choice =>
+                        <ModalDialogButton
+                            key={choice}
+                            dialog={state.activeDialog!}
+                            choice={choice}
+                            appState={appState}
+                            setAppState={setAppState}
+                        />
+                    )}
+                </div>
+            </div>
+        </div>
     </div>;
+}
+
+interface ModalDialogButtonProps<C extends readonly string[]> {
+    dialog: Dialog<C>,
+    choice: C[number],
+    appState: AppState,
+    setAppState(_: AppState): void,
+}
+
+function ModalDialogButton<C extends readonly string[]>({
+    dialog,
+    choice,
+    appState,
+    setAppState,
+}: ModalDialogButtonProps<C>) {
+    return <a
+        //key={choice}
+        className="Visualization__Modal__Dialog__Button"
+        onClick={_ => {
+            const state = appState.vmState.last() ?? State.new();
+            const prevState = state.setActiveDialog(null);
+            try {
+                setAppState({...appState, vmState: appState.vmState.push(dialog.apply(choice, prevState))});
+            } catch (ex) {
+                if (!(ex instanceof ExecutionError)) {
+                    console.error("Internal error!", ex);
+                }
+                const message = ex instanceof Error ? ex.message : JSON.stringify(ex);
+                setAppState({...appState, lastExecutionError: ex instanceof ExecutionError ? ex : message});
+            }
+        }}
+    >{choice}</a>;
 }
 
 function generateLayout(state: State, graph: Graph): FcoseLayoutOptions {
@@ -69,7 +122,11 @@ function generateLayout(state: State, graph: Graph): FcoseLayoutOptions {
     };
 }
 
-function VisualizationGraph({state}: VisualizationProps) {
+interface VisualizationGraphProps {
+    state: State,
+}
+
+function VisualizationGraph({state}: VisualizationGraphProps) {
 
     const cyRef = useRef<Cytoscape.Core>();
 
