12cfbfda6fe94fe6f642b30e2865637f103f5396
hannahkiel <hannah.kiel@tum.de>
2022-11-23 15:28:59 +0100

    Init problem fixed, hopefully + Label init

--- src/__tests__/Parser.test.ts
+++ src/__tests__/Parser.test.ts
@@ -5,7 +5,7 @@ test("parsing", () => {
     //let program = Parsing.parse("t(X) <= X = b q(X) <= s(X) <= X = b ");
     //let program = Parsing.parse("b(X) <= X = f(X,Y) <= X = f(a, b, _)");
    //let program = Parsing.parse("t(X) <= X = b <= t(X)");
-    let program = Parsing.parse("t(X) <= X = b q(X) <= s(X) s(X) <= X = a p() <= q(X),t(X) s(X) <= t(X) <= p()");
+    let program = Parsing.parse("t(X) <= X = b q(X)   <= s(X) s(X) <= X = a p() <= q(X),t(X) s(X) <= t(X) <= p()");
 
 
     let code: CodeGenerator = new CodeGenerator();
