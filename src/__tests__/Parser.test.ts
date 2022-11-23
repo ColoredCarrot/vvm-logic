f818cc14280c256778774e0541af3dac43a4d016
hannahkiel <hannah.kiel@tum.de>
2022-11-23 17:44:52 +0100

    Tests

--- src/__tests__/Parser.test.ts
+++ src/__tests__/Parser.test.ts
@@ -4,13 +4,10 @@ import {CodeGenerator} from "../parser/CodeGenerator";
 test("parsing", () => {
     //let program = Parsing.parse("t(X) <= X = b q(X) <= s(X) <= X = b ");
     //let program = Parsing.parse("b(X) <= X = f(X,Y) <= X = f(a, b, _)");
-    //let program = Parsing.parse("t(X) <= X = b <= t(X)");
     const program = Parsing.parse("t(X) <= X = b q(X)   <= s(X) s(X) <= X = a p() <= q(X),t(X) s(X) <= t(X) <= p()");
-
 
     const code: CodeGenerator = new CodeGenerator();
     console.log(code.code_Program(program));
-    //b(X) <= X = f(X,Y) <= X = f(a, b, _)
-    //t(X) <= X = b q(X) <= s(X) <= X = b --> Variablen problem
+
 });
 
