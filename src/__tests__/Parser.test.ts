3611622d5c8d093ffb3dab74a5bb0e335deeda9f
hannahkiel <hannah.kiel@tum.de>
2022-11-24 11:51:48 +0100

    parser/ lintfix

--- src/__tests__/Parser.test.ts
+++ src/__tests__/Parser.test.ts
@@ -4,21 +4,11 @@ import {CodeGenerator} from "../parser/CodeGenerator";
 test("parsing", () => {
     //let program = Parsing.parse("t(X) <= X = b q(X) <= s(X) <= X = b, X = a");
     //let program = Parsing.parse("b(X) <= X = f(X,Y) <= X = f(a, b, _)");
-    //const program = Parsing.parse("t(X) <= X = b q(X)   <= s(X) s(X) <= X = a p() <= q(X),t(X) s(X) <= t(X) <= p()");
-
-    const program = Parsing.parse("bigger(X,Y) <= X = elephant, Y = horse <= b(X,Y)");
+    const program = Parsing.parse("t(X) <= X = b q(X)   <= s(X) s(X) <= X = a p() <= q(X),t(X) s(X) <= t(X) <= p()");
+    //const program = Parsing.parse("bigger(X,Y) <= X = elephant, Y = horse <= b(X,Y)");
 
     const code: CodeGenerator = new CodeGenerator();
     console.log(code.code_Program(program));
 
 
 });
-/*
-bigger(X,Y) :- X = elephant, Y = horse.
-bigger(X,Y) :- X = horse, Y = donkey.
-bigger(X,Y) :- X = donkey, Y = dog.
-bigger(X,Y) :- X = donkey, Y = monkey.
-is_bigger(X,Y) :- bigger(X,Y).
-is_bigger(X,Y) :- bigger(X,Z), is_bigger(Z,Y).
-
- */
